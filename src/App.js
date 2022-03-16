91e0811cb70ffbf5b072e7c4b304b264434e8997
Nidhi Tholar <nidhi.tholarkuchur@sjsu.edu>
2022-03-16 15:03:45 -0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// const App = ({title}) =>(
+// <div>{title}</div>
+// )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import axios from 'axios';
+
+
+// const testData = [
+//   {name:"Nidhi", company:"EY"},
+//   {name:"John", company:"Google"}
+// ];
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+
+    {/* {testData.map(profile => <Card {...profile}/>)} */}
+
+    {/* <Card {...testData[0]}/>
+    <Card {...testData[1]}/> */}
+
+  </div>
+)
+
+class Card extends React.Component{
+  render(){
+    const profile = this.props;
+    return (
+      <div>
+        <img src={profile.avatar_url}/>
+        <div>
+          <div style={{fontSize:'42px'}}>{profile.name}</div>
+          <div>{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component{
+
+  // userNameInput = React.createRef(); //not using Ref
+  state = {userName:''};
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`); //axios.get returns a promise
+    this.props.onSubmit(resp.data);
+    this.setState({userName:''});
+  };
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {/* <input type="text" placeholder="Github username" ref={this.userNameInput} required></input> */}
+        <input type="text" placeholder="Github username" value={this.state.userName} onChange={event => this.setState({userName:event.target.value})} required></input>
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     profiles: testData
+  //   };
+  // }
+
+  state = {
+    profiles:[]
+  };
+
+  addNewProfile = (profileData) => {
+    console.log('App', profileData)
+    this.setState(prevState=>({
+      profiles:[...prevState.profiles, profileData]
+    }));
+  }
+
+  render(){
+    return (
+    <div>
+      <div>{this.props.title}</div>
+      <Form onSubmit={this.addNewProfile}/>
+      <CardList profiles={this.state.profiles}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
